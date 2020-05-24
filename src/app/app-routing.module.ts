e08184afcb17b3de5359f4311c9a43539174ef9b
juniorpires <juniorpiresupe@gmail.com>
2020-05-24 16:34:47 -0300

    mapa2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { UnidadeAdmComponent } from './unidade-adm/unidade-adm.component';
 
 
 const routes: Routes = [
-  //{ path: '',  component: MapComponent },
+  { path: '',  component: MapComponent },
   { path: 'mapa',  component: MapComponent },
   { path: 'unidades',  component: UnidadeAdmComponent },
   { path: 'situacao',  component: SituacaoComponent },
